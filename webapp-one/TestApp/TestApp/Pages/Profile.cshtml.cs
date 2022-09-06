67642a600d08bf29cd00141e5ca141f16472c730
JJ Escandor <jj.p.escandor@gmail.com>
2022-09-05 21:55:27 -0400

    T1

--- webapp-one/TestApp/TestApp/Pages/Profile.cshtml.cs
+++ webapp-one/TestApp/TestApp/Pages/Profile.cshtml.cs
@@ -4,24 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace TestApp.Pages
 {
-    public class IndexModel : PageModel
+    public class ProfileModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
-
-
         public void OnGet()
         {
-
         }
     }
 }
